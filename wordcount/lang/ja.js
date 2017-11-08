1519a8a9eec76f5e7767c1fbee1e807f0c6c794a
g_ryotaro <goto@basicinc.jp>
2017-11-08 22:06:58 +0900

    日本語追加

--- wordcount/lang/ja.js
+++ wordcount/lang/ja.js
@@ -4,10 +4,13 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang('wordcount', 'ja', {
     WordCount: '単語数:',
+    WordCountRemaining: '語追加可能',
     CharCount: '文字数:',
-    CharCountWithHTML: '文字数 (with HTML):',
-    Paragraphs: 'Paragraphs:',
-    pasteWarning: 'Content can not be pasted because it is above the allowed limit',
-    Selected: 'Selected: ',
+    CharCountRemaining: '字追加可能',
+    CharCountWithHTML: '文字数 (HTMLタグを含む):',
+    CharCountWithHTMLRemaining: '字追加可能 (HTMLタグを含む):',
+    Paragraphs: '段落数:',
+    pasteWarning: '文字数/単語数の制限を超えるため、貼り付けできません。',
+    Selected: '選択中の字数:',
     title: 'ワードカウント'
 });
